e87bc6facd368734c002eef456565cf9e329a0b6
Daniel Delgado <daniel.delgado@onetree.com>
2022-12-13 14:14:05 -0300

    Updating entities

--- src/api/project-id/routes/project-id.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-id router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::project-id.project-id');
--- src/api/project-id/services/project-id.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project-id service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::project-id.project-id');
